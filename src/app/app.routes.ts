123734ce04a6c70e10e51bd804e06c6343c2c1c5
Eduardo-Del-Aguila <eduardodelaguila10@gmail.com>
2025-05-13 15:28:12 -0500

    Antes de implementar API spotify

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+  { path: 'home',
+    loadComponent:()=> import('./components/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'search',
+    loadComponent: () => import('./components/search/search.component').then(m => m.SearchComponent)
+  },
+  {
+    path: 'artistt',
+    loadComponent: () => import('./components/artist/artist.component').then(m => m.ArtistComponent)
+  },
+  {
+    path: '', pathMatch: 'full', redirectTo: 'home'
+  },
+  {
+    path: '**', pathMatch: 'full', redirectTo: 'home'
+  }
+
+];
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.component.html'
+})
+export class HomeComponent {
+
+  miPais: any[]=[]
+  constructor( ){
+
+    }
+  }
+
